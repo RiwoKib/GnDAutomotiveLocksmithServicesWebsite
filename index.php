88661b9b6740b2a9c765bc19db2c9dafd8224aa9
Riwokib <riwellkib@gmail.com>
2023-09-11 23:19:31 +0300

    banner slider

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include 'includes/header.php'?>
+
+
+<!-- BANNER SECTION -->
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="assets/img/banner3.jpg" class="d-block w-100">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="assets/img/banner2.jpg" class="d-block w-100 opacity-10">
+      <div class="carousel-caption d-none d-md-block">
+        <h5  class="text-secondary-emphasis">Looking for professional car locksmith services</h5>
+        <p>We've got you covered. Contact us today!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/banner1.jpg" class="d-block w-100">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<?php include 'includes/footer.php'?>
