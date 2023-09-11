b2a165de542cee25ab1df85d784b551b572f0f7e
Riwokib <riwellkib@gmail.com>
2023-09-12 00:29:54 +0300

    FAQs | Qualities section

--- index.php
+++ index.php
@@ -32,4 +32,180 @@
   </button>
 </div>
 
+<!-- SERVICES SECTION -->
+<div class="container">
+    <div class="row pb-5">
+        <div class="col-md-12">
+            <hr>
+            <h5 class="text-info">We provide a wide variety of car locksmith services in Karioko, Nairobi CBD outskirts</h5>
+            <h2><strong>GnD automotive locksmith services</strong></h2>
+        </div>
+    </div>
+
+    <div class="row mb-5 justify-content-center">
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/icon1.jpg" class="w-25 ps-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Lost car keys?</strong></h5>
+                    <p class="card-text">Have you lost all of your automotive key? Do not worry! We can supply, cut, and program new transponder keys. The best part is that we offer a mobile service!</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Yes, I need this service. Lets book</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/icon1.jpg" class="w-25 ps-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Want car key duplicated?</strong></h5>
+                    <p class="card-text">Are you left with only one set of keys? Need key duplicated? We provide car key duplication services. We cover any car brand and most of the models.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Yes, I need this service. Lets book</a>
+                </div>
+            </div>
+        </div> 
+
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/icon1.jpg" class="w-25 ps-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Require ignition lock fixed?</strong></h5>
+                    <p class="card-text">Does your key no longer operate the ignition cylinder? Our professional auto locksmiths can fix it for you.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Yes, I need this service. Lets book</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img src="assets/img/icon1.jpg" class="w-25 ps-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Need car remote made?</strong></h5>
+                    <p class="card-text">We provide car key replacement services for any vehicle. Our locksmiths can generate all types of car remotes, such as remote keys, smart keys, and key fobs.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Yes, I need this service. Lets book</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-3 mt-5">
+            <button class="btn btn-outline-primary w-100">View all Services</button>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <hr>
+            <h5 class="text-info text-center">Discover why we are the right people to hire</h5>
+            <h2 class="text-center"><strong>Why do people choose us?</strong></h2>
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center align-items-center">
+        <div class="col-6">
+            <h2 class="text-center"><strong>1. Quality and Professionalism</strong></h2>
+            <p>Providing the best car key services is our top priority, so we stock our shop with high-quality car keys and equip our vans with the latest cutting and programming machines.
+
+            Just tools won't make you an expert, you will need experience too. This is why we are investing time and resources into educating our staff on the latest programming methods and protective measures related to automotive security.
+
+            When you call CarKeyAction, you know you're calling an expert.</p>
+        </div>
+
+        <div class="col-5"> 
+            <img class="w-100 rounded" src="assets/img/hire1.jpg"> 
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center align-items-center">
+        <div class="col-5"> 
+            <img class="w-100 rounded" src="assets/img/hire1.jpg"> 
+        </div>
+
+        <div class="col-6">
+            <h2 class="text-center"><strong>1. Quality and Professionalism</strong></h2>
+            <p>Providing the best car key services is our top priority, so we stock our shop with high-quality car keys and equip our vans with the latest cutting and programming machines.
+
+            Just tools won't make you an expert, you will need experience too. This is why we are investing time and resources into educating our staff on the latest programming methods and protective measures related to automotive security.
+
+            When you call CarKeyAction, you know you're calling an expert.</p>
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center align-items-center">
+        <div class="col-6">
+            <h2 class="text-center"><strong>1. Quality and Professionalism</strong></h2>
+            <p>Providing the best car key services is our top priority, so we stock our shop with high-quality car keys and equip our vans with the latest cutting and programming machines.
+
+            Just tools won't make you an expert, you will need experience too. This is why we are investing time and resources into educating our staff on the latest programming methods and protective measures related to automotive security.
+
+            When you call CarKeyAction, you know you're calling an expert.</p>
+        </div>
+
+        <div class="col-5"> 
+            <img class="w-100 rounded" src="assets/img/hire1.jpg"> 
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center">
+         <div class="col-md-12">
+            <hr> 
+            <h2 class="text-center"><strong>Frequently Asked Questions</strong></h2>
+            <h5 class="text-info text-center">Answers to the most FAQs</h5>
+        </div>
+        <div class="col-6">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Can you help me if I lost all of my car keys?
+                    </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>Yes,</strong> in a situation where you lost all of your car keys, our expert would come to your location and retrieve your key combination for your car.  
+                        He would cut new keys accordingly and program them to your vehicle.  We make all types of car keys, remote keys, push start keys, regular chipped keys, and more! 
+                        You could visit our blog post Lost all my car keys for more information.  Or contact us at <strong>0742-874-651</strong>, and we would be happy to assist you!
+                    </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Is it true that only the dealership can make new car keys?
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>No.</strong> In most cases, the dealership will tell it to you to lure you into the dealership to catch the sale.   
+                        However, a competent car locksmith can provide new car keys to almost any vehicle, and he will do it without compromising on the quality of the end product. 
+                        Contact us today for more information.
+                    </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Can a locksmith make a key for a car ignition?
+                    </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>Yes!</strong> An automotive locksmith can create a new key that works on your car's ignition.  
+                        Not only that, an automotive locksmith can reprogram your key fob or replace your car ignition if that is the issue. 
+                        Are you facing this issue? Call 0742-874-651 for more details.
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php include 'includes/footer.php'?>
