46a02c227527f7c2fdab264d35a813d316e0e8d4
Riwokib <riwellkib@gmail.com>
2023-09-11 21:41:48 +0300

    include navbar file

---
+++ includes/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="assets/images/GnDlogo.jpg" alt="logo" width="150px" height="30px">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end me-5" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#"> Chip Keys and Transponder Keys Replacement</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Programming</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Programming</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Programming</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Programming</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Programming</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Programming</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
