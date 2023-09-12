aac7c05fa254c20d75b8cd767d54ac6545b031ad
Riwokib <riwellkib@gmail.com>
2023-09-12 21:17:27 +0300

    includes files

---
+++ includes/navbar.php
@@ -0,0 +1,45 @@
+<nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+        <img src="assets/img/GnDlogo.jpg" alt="logo" width="150px" height="30px">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end me-5" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Chip Keys and Transponder Keys Replacement</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Programming</a></li>
+            <li><a class="dropdown-item" href="#">Car Key Replacement</a></li>
+            <li><a class="dropdown-item" href="#">Laser Cut Keys</a></li>
+            <li><a class="dropdown-item" href="#">Motorcycle Key Replacement</a></li>
+            <li><a class="dropdown-item" href="#">Ignition Lock Repair</a></li>
+            <li><a class="dropdown-item" href="#">Door Lock Repair</a></li>
+            <li><a class="dropdown-item" href="#">Car Lockout Services</a></li>
+            <li><a class="dropdown-item" href="#">Car Tracker & Alarm Installation</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <li class="nav-item me-5">
+          <a class="nav-link" href="blog.php">Blog</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
