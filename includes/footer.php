fc7047b29e45717ef197354f85d2df5d5d8ec6ea
Riwokib <riwellkib@gmail.com>
2023-09-12 17:15:50 +0300

    footer section

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,70 @@
-    
+   <footer>
+    <div class="row bg-dark p-5">
+          <div class="col-4">
+              <img src="assets/img/GnDlogo.jpg" alt="logo">
+              <p class="text-white pt-3">Automotive locksmith services</p>
+              <div class="text-white">
+                <span class="px-2"><i class="fa fa-2x fa-facebook-square"></i>
+                </span>
+                <span class="px-2"><i class="fa fa-2x fa-instagram"></i>
+                </span>
+                <span class="px-2"><i class="fa fa-2x fa-twitter-square"></i>
+                </span>
+                <span class="px-2"><i class="fa fa-linkedin-square fa-2x"></i>
+                </span>
+                <span class="px-2"><img style="margin-top: -10px" width="35px" height="35px" src="assets/img/tiktok.png" alt="tiktok icon"></span>
+                <span class="px-2"><i class="fa fa-envelope fa-2x"></i>
+                </span>
+                <span class="px-2"><i class="fa fa-whatsapp fa-2x"></i>
+                </span>
+              </div>
+          </div>
+
+          <div class="col-2">
+              <h4 class="text-white">General Menu</h4>
+              <ul class="">
+                  <li><a href="">Home</a>
+                  </li>
+                  <li><a href="">Services</a></li>
+                  <li><a href="">Contact</a></li>
+                  <li><a href="">About</a></li>
+                  <li><a href="">Blog</a></li>
+              </ul>
+          </div>
+
+          <div class="col-3">
+              <h4 class="text-white">Services</h4>
+              <ul class="">
+                <li><a class="" href="#">Chip Keys and Transponder Keys Replacement</a></li>
+                <li><a class="" href="#">Car Key Programming</a></li>
+                <li><a class="" href="#">Car Key Replacement</a></li>
+                <li><a class="" href="#">Laser Cut Keys</a></li>
+                <li><a class="" href="#">Motorcycle Key Replacement</a></li>
+                <li><a class="" href="#">Ignition Lock Repair</a></li>
+                <li><a class="" href="#">Door Lock Repair</a></li>
+                <li><a class="" href="#">Car Lockout Services</a></li>
+                <li><a class="" href="#">Car Tracker & Alarm Installation</a></li>
+              </ul>
+          </div>
+
+          <div class="col-3 text-white">
+              <h4 class="text-white">Contact</h4>
+              <ul class="">
+                  <h2><a href="tel:0742874651">0742-874-651</a>
+                  </h2>
+                  <button class="my-3 btn btn-warning btn-lg">Book an Appointment</button>
+                  <h5>info@GnDAutoLocksmith.co.ke</h5>
+                  <p>Service area: Karioko, Langata SouthLands, Mbagathi Road, City Stadium.</p>
+              </ul>
+          </div>
+      </div>
+
+      <div class="row ps-5 bg-info-subtle pt-2  ">
+        <p>&copy; <strong class="text-info-subtle">copyright 2024</strong><span class="text-muted"> GnDAutomotiveLocksmithServices</span>
+      </div>
+   </footer> 
    
+
     <!-- Custom scripts -->
     <script src="assets/js/app.js"></script>
      <!-- BOOTSTRAP -->
