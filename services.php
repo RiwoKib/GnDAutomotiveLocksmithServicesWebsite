9ad763858836e89e646fb54ee20a1c64d3531b80
Riwokib <riwellkib@gmail.com>
2023-09-12 20:53:59 +0300

    banner | all services section

---
+++ services.php
@@ -0,0 +1,160 @@
+<?php include "includes/header.php"?>
+
+
+<div class="opacity-25">
+    <img class="w-100" src="assets/img/banner1.jpg">
+
+    <div class="row bg-transparent justify-content-center">
+        <div class="col-6">
+            <h1 class="text-muted text-center"><strong>Affordable and Reliable Automotive Locksmith GnDAuto</strong></h1>
+        </div>
+    </div>
+</div>
+
+<!-- CALL TO ACTION -->
+<div class="row py-3 px-5 bg-dark justify-content-center align-items-center">
+    <div class="col-7 text-white">
+        <p>When you lose your auto keys, it can be a stressful situation. We are a reputable car locksmith in Calgary AB, offering a variety of stress-free vehicle locksmith services in Calgary and surrounding areas, including emergency & non-emergency services. We come to your doorstep, so you are never left out in the cold. Our team is always on hand to help you out.</p>
+    </div>
+
+    <div class="col-4">
+        <a href="contact.php"><button class="p-3 btn btn-lg btn-warning float-end">Book An Appointment</button></a>
+    </div>
+</div>
+
+<!-- ALL SERVICES -->
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center">
+            <h2 class=""><strong>Automotive locksmith services we provide</strong></h2>
+            <h5 class="text-info">Our main car key services</h5>
+            <hr>
+        </div>
+    </div>
+
+    <div class="row mb-5 justify-content-center">
+         
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/keyduplicate.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Car Key Duplication</strong></h5>
+                    <p class="card-text"><strong class="text-warning">Old key acting up?</strong> Have only one function key for your vehicle and need your car key duplicated? We can provide a car key or fob replacement to any key type of vehicle.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+       
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/motorcyclekey.jpg" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Motorcycle Key Replacement</strong></h5>
+                    <p class="card-text">Lost all the keys to your motorcycle? Need a motorcycle key duplicate and nobody does it in the city? Don't worry! We can duplicate or generate keys from scratch to almost any motorcycle. Including advanced chipped motorycle keys.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+       
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/lockoutservice.jpg" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Car Lockout Services</strong></h5>
+                    <p class="card-text">Locked your Keys inside your car? We know how stressful it can be, but you shouldn't worry. All of our technicians are equipped with car opening tools and they will be able to assist you in no time.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/remotereplacement.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Car Remote Replacement</strong></h5>
+                    <p class="card-text">Almost all new cars are now coming with remote options from the manufacture. One benefit of this is being able to lock and unlock the doors remotely, as well as other options depending on what your car comes with. We carry quality remotes and can program them for you.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/lasercut.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Laser Cut Keys</strong></h5>
+                    <p class="card-text">Newer vehicles are equipped with higher security keys. Those keys require special machines to cut them. Our technicians are equipped with the most advanced laser cutting machines to get the job done. We are able to cut even the newest car key blades.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/transponderkeys.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Chip Keys / Transponder Keys</strong></h5>
+                    <p class="card-text">Chipped car keys have become more complicated over the years, and so has the car key programming procedure. We're keeping up with the developing market and able to provide chipped keys to almost any vehicle, even if you lost all your spare keys.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/ignitionrepair.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Ignition Lock Repair</strong></h5>
+                    <p class="card-text">Ignition key stopped turning the ignition? Your ignition is rough and hard to operate? We can fix it! This issue is very common on Honda vehicles but can happen to any other vehicles. We are able to repair or replace almost any type of ignition lock cylinder.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/doorepair.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Door Lock Repair</strong></h5>
+                    <p class="card-text">Door lock stopped working? You want to change the old combination on your door locks? We provide car door lock rekey solutions, door cylinder repairs and door lock replacements services.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/carkeyprogramming.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Car Key Programming</strong></h5>
+                    <p class="card-text">Automobile keys that are ordered online need to be programmed to match the vehicle. This requires advanced programming machines. We provide car key and fob programming services. A car locksmith GnD you can trust.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div> 
+        
+    </div>
+</div>
+
+
+<?php include "includes/footer.php"?>
