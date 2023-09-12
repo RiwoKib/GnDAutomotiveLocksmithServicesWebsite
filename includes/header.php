1d03aadaf5be443028a696593dddc19262ae4dc0
Riwokib <riwellkib@gmail.com>
2023-09-12 17:31:47 +0300

    fontawesome cdn link

--- includes/header.php
+++ includes/header.php
@@ -8,10 +8,7 @@
     <!-- Favicon -->
     <link rel="icon" href="assets/img/mdb-favicon.ico" type="image/x-icon" />
     <!-- Font Awesome -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    />
+    <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
     <!-- Google Fonts Roboto -->
     <link
       rel="stylesheet"
