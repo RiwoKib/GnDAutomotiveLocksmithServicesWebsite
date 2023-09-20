1250f6b40bd611a2899ab0e0b9a345ac8a3f9696
Riwokib <riwellkib@gmail.com>
2023-09-20 23:19:54 +0300

    banner | abt desc | what we do  | contact  section

---
+++ about.php
@@ -0,0 +1,273 @@
+<?php include "includes/header.php"?>
+
+
+
+<div class="bg-image">
+    
+    <img class="w-100 bg-img" src="assets/img/banner1.jpg">
+    
+</div>
+
+<!-- CALL TO ACTION -->
+<div class="row py-3 px-5 bg-dark justify-content-center align-items-center">
+    <div class="col-7 text-white">
+        <p>Losing your car keys can be stressful, but you can rest assured that we will be able to help you! We provide a quick and reliable service that will get your car keys replaced in no time. We come to your location and do everything on site. CarKeyAction is always on hand to help you out. Don't hesitate and call us today at <strong>0742-874-651</strong> GnDAutoLocksmith you can trust.</p>
+    </div>
+
+    <div class="col-4">
+        <a href="contact.php"><button class="p-3 btn btn-lg btn-warning float-end">Book An Appointment</button></a>
+    </div>
+</div>
+
+
+<div class="container my-5">
+    <div class="row">
+        <div class="col-6">
+            <h1>About <span class="text-info font-bold">GnDAutoLocksmith</span></h1>
+            <hr>
+            <p>Automotive locksmith is a tough industry. Today it is not an easy task to find a competent car locksmith who you can trust.
+
+            When you finally feel like you've found the right company, they may end up damaging your car and not returning any of your calls, leaving you to deal with the ensuing problem. It can be really frustrating and we know it.
+
+            We're here to make your life easier and prevent scenarios like this.
+
+            We at GnDAutoLocksmith provide a quick solution for all your car key needs. A reliable company you can rely on.
+
+            All our technicians are experts in the automotive locksmith industry, coming equipped with the best tools on the market to cover as many cars as possible. They are committed to providing you with quality car key services without compromise.
+            <br>
+            <br>
+            <em>We are CarKeyAction Calgary's trusted car locksmiths.</em>
+        </p>
+        </div>
+
+        <div class="col-6">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="w-100" src="assets/img/keyduplicate.jfif" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="w-100" src="assets/img/ignitionrepair.jfif" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="w-100" src="assets/img/transponderkeys.jfif" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="bg-primary text-center">
+    <h1 class=" text-white fw-bold">We like what we do, and we do it well.</h1>
+    <hr class="">
+
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active ">
+                <div class="d-flex justify-content-between p-5">
+                    <div class="card" style="width: 15rem;">    
+                        <div class="card-body">
+                            <h5 class="card-title">Toyota Corolla 2010</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Key Duplication</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Toyota Corolla 2010</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Key Cutting</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Toyota Corolla 2015</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Transponder Key Programming</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Toyota Hilux </h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Lockout Services</h6>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Toyota Land Cruiser Prado</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Trunk Unlocking</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item ">
+                <div class="d-flex justify-content-between p-5">
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Nissan X-Trail 2009 </h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Remote Start Installation</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Nissan X-Trail 2009 </h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Remote Start Installation</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Nissan X-Trail 2015</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Remote Start Installation</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Nissan Serena C25</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Remote Start Installation</h6>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Nissan Navara NP300</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Remote Start Installation</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item ">
+                <div class="d-flex justify-content-between p-5">
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Subaru Outback 2010</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Key Fob Replacement & Programming</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Subaru Outback 2010</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Key Fob Replacement & Programming</h6>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Subaru Outback 2015</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Key Fob Replacement & Programming</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Subaru Foresta SH</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Ignition Repair</h6>
+                        </div>
+                    </div> 
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Subaru Impreza 2015</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Ignition Repair</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+    </div>
+
+    <div class="py-5">
+        <a href="contact.php"><button class="p-3 btn btn-lg bg-dark text-white btn-warning">Book An Appointment</button></a>
+    </div>
+</div>
+
+<section class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <hr>
+            <h5 class="text-info text-center">Discover why we are the right people to hire</h5>
+            <h2 class="text-center"><strong>Why do people choose us?</strong></h2>
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center align-items-center">
+        <div class="col-6">
+            <h2 class="text-center"><strong>1. Quality and Professionalism</strong></h2>
+            <p>Providing the best car key services is our top priority, so we stock our shop with high-quality car keys and equip our vans with the latest cutting and programming machines.
+
+            Just tools won't make you an expert, you will need experience too. This is why we are investing time and resources into educating our staff on the latest programming methods and protective measures related to automotive security.
+
+            When you call CarKeyAction, you know you're calling an expert.</p>
+        </div>
+
+        <div class="col-5"> 
+            <img class="w-100 rounded" src="assets/img/hire1.jpg"> 
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center align-items-center">
+        <div class="col-5"> 
+            <img class="w-100 rounded" src="assets/img/hire1.jpg"> 
+        </div>
+
+        <div class="col-6">
+            <h2 class="text-center"><strong>1. Quality and Professionalism</strong></h2>
+            <p>Providing the best car key services is our top priority, so we stock our shop with high-quality car keys and equip our vans with the latest cutting and programming machines.
+
+            Just tools won't make you an expert, you will need experience too. This is why we are investing time and resources into educating our staff on the latest programming methods and protective measures related to automotive security.
+
+            When you call CarKeyAction, you know you're calling an expert.</p>
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center align-items-center">
+        <div class="col-6">
+            <h2 class="text-center"><strong>1. Quality and Professionalism</strong></h2>
+            <p>Providing the best car key services is our top priority, so we stock our shop with high-quality car keys and equip our vans with the latest cutting and programming machines.
+
+            Just tools won't make you an expert, you will need experience too. This is why we are investing time and resources into educating our staff on the latest programming methods and protective measures related to automotive security.
+
+            When you call CarKeyAction, you know you're calling an expert.</p>
+        </div>
+
+        <div class="col-5"> 
+            <img class="w-100 rounded" src="assets/img/hire1.jpg"> 
+        </div>
+    </div>
+
+    <!-- CONTACT US SECTION -->
+    <div class="row my-5 justify-content-center">
+        <div class="col-6">
+            <h2 class="fw-bold">Contact Us</h2>
+            <hr class="col-2">
+            <div class="row">
+                <div class="col-6">
+                <p>Have a question? Looking for a quote? Send us a message and we'll be in touch pronto.</p>
+
+                <h4 class="fw-bold">Area of Service</h4>
+                <p>Nairobi Karioko, Langata, Mbagathi Road, Jogoo Road.</p>
+
+                <h4>Business Hours</h4>
+                <p>Mon-Sat 7:00am - 7:00pm <br><strong class="text-danger">Emergency Services</strong> available.
+                <br>
+                (Open Ups)</p>
+                </div>
+                <div class="col-4 d-flex ">
+                    <div>
+                        <i class="fa text-warning fa-envelope-o fa-4x"></i>
+                        <h5 class="fw-bold text-primary">Email Us</h5>
+                        <a class="text-muted" href="">Send Us a Message</a>
+                    </div>
+                    <div class="text-nowrap">
+                        <i class="fa text-warning fa-phone fa-4x"></i>
+                        <h5 class="fw-bold text-primary">Call Us</h5>
+                        <a class="text-muted" href="">0742-874-651</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <img src="assets/img/contactlostkey.png">
+        </div>
+    </div>
+</section>
+
+<?php include "includes/footer.php"?>
