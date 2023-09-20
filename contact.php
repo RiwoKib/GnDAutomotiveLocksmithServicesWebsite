4778f25f42fbf5e9bd36796487a082f979a3be5f
Riwokib <riwellkib@gmail.com>
2023-09-21 01:04:40 +0300

    banner | form | listing, policies | map section

---
+++ contact.php
@@ -0,0 +1,182 @@
+<?php include "includes/header.php"?>
+
+<div class="">
+    
+    <img class="w-100 bg-img" src="assets/img/banner1.jpg">
+    
+</div>
+<div class="">
+    <div class="row  justify-content-center text-center">
+        <div class="col-6">
+            <h1 class="text-danger"><strong>Lost Your Car Keys? Need a Car Locksmith? Contact Us!</strong></h1>
+            <h6>Call now <strong class="text-primary fw-bold">(0742)-874-651</strong> for immediate service!</h6>
+        </div>
+    </div>
+</div>
+
+<!-- CONTACT FORM SECTION -->
+
+<div class="container ">
+    <div class="row justify-content-center">
+        <div class="col-5">
+            <h1 class="fw-bold">Get In Touch</h1>
+            <hr class="col-3 ">
+            <p>GnDAutoLocksmith professional auto locksmith and car key replacement service provider in Nairobi. Feel free to call us at <strong class="text-primary fw-bold">(0742)-874-651</strong> or fill out our form below, and one of our representatives will help you solve your car key problem immediately.</p>
+
+            <!-- FORM -->
+            <form class="row g-3">
+                <div class="col-md-6">
+                    <label for="Name" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="Name">
+                </div>
+                <div class="col-md-6">
+                    <label for="Phone" class="form-label">Phone Number</label>
+                    <input type="text" class="form-control" id="Phone">
+                </div>
+                <div class="col-12">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="example@gmail.com">
+                </div>
+                
+                <div class="col-md-3">
+                    <label for="Car Make" class="form-label">Car Make</label>
+                    <select id="carmake" class="form-select">
+                    <option selected>Choose...</option>
+                    <option>Toyota</option>
+                    <option>Hyundai</option>
+                    <option>Subaru</option>
+                    <option>Suzuki</option>
+                    <option>Volvo</option>
+                    <option>Honda</option>
+                    </select>
+                </div>
+                
+                <div class="col-md-3">
+                    <label for="Car Model" class="form-label">Car Model</label>
+                    <input type="text" class="form-control" id="Car Model">
+                </div>
+
+                <div class="col-md-3">
+                    <label for="Year" class="form-label">Year</label>
+                    <select id="Year" class="form-select">
+                    <option selected>Choose...</option>
+                    <option>2010</option>
+                    <option>2011</option>
+                    <option>2012</option>
+                    <option>2013</option>
+                    <option>2014</option>
+                    <option>2015</option>
+                    <option>2016</option>
+                    <option>2017</option>
+                    <option>2018</option>
+                    <option>2019</option>
+                    </select>
+                </div>
+
+                <div class="col-md-3">
+                    <label for="inputState" class="form-label">Key Status</label>
+                    <select id="inputState" class="form-select">
+                    <option selected>Choose...</option>
+                    <option>Have Lost all Keys</option>
+                    <option>Have Original Keys</option>
+                    <option>Have Broken Original Keys</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Describe Your Problem</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+
+                <div class="row ">
+                    <div class="col-md-5">
+                        <label for="Location" class="form-label">Where Can We Meet?</label>
+                        <input type="text" class="form-control" id="Location">
+                    </div>
+
+                    <div class="col-4 pt-5">
+                        <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">
+                            Get Reminders
+                        </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mb-4">
+                    <button type="submit" class="w-100 btn btn-primary">Send</button>
+                </div>
+            </form>
+
+        </div>
+        <div class="col-5 p-5 bg-dark-subtle m-5 shadow-lg rounded">
+            <div>
+                <h4 class="fw-bold"><span><i class="fa text-primary fa-phone fa-2x"></i></span> Phone Number</h4>
+                <p class=" ms-2 ps-5">0742-874-651</p>
+            </div>
+            <div>
+                <h4><span><i class="fa text-primary fa-clock-o fa-2x"></i></span> Business Hours</h4>
+                <p class=" ms-2 ps-5">Mon-Sat 7:00am - 7:00pm <br><strong class="text-danger">Emergency Services</strong> available.
+                <br>
+                (Open Ups)</p>
+            </div>
+            <div>
+                <h4><span><i class="fa text-primary fa-envelope-o fa-2x"></i></span> Email Address</h4>
+                <p class=" ms-2 ps-5">info@GnDAuto.co.ke</p>
+            </div>
+            <div>
+                <h4><span><i class="fa text-primary fa-map-marker fa-2x"></i></span> Area of Service</h4>
+                <p class=" ms-2 ps-5">Nairobi Karioko, Langata, Mbagathi Road, Jogoo Road.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center my-5">
+        <div class="col-5 d-flex align-items-center">
+            <img src="assets/img/Greviews.jpeg" width="150px" height="100px">
+            <div class="bg-dark-subtle p-3">
+                <p class="pt-3">Check our <a href="">Google Listing</a>.</p>
+            </div>
+        </div>
+        <div class="col-5 d-flex align-items-center">
+            <img src="assets/img/Freviews.jpg" width="150px" height="100px">
+            <div class="bg-dark-subtle p-3">
+                <p class="pt-3">Check our <a href="">Facebook Listing</a>.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row row-cols-2 justify-content-center">
+        <div class="col-5 m-1 bg-primary bg-gradient rounded shadow-sm p-4">
+            <h4 class="text-white fw-bold">Fair and transparent pricing</h4>
+            <p class="text-muted ">We offer straightforward pricing and policy. You will always know what you will be paying before any work is done. You will never have to worry about hidden fees from our locksmith company. </p>
+        </div>
+        <div class="col-5 m-1 bg-primary bg-gradient rounded shadow-sm p-4">
+            <h4 class="text-white fw-bold">Easy Booking Process</h4>
+            <p class="text-muted">1 min to get a quote and book a service online or over the phone. Our automotive locksmith will come to you whenever your car is in Calgary or the surrounding areas.</p>
+        </div>
+        <div class="col-5 m-1 bg-primary bg-gradient rounded shadow-sm p-4">
+            <h4 class="text-white fw-bold">Quick Response Time</h4>
+            <p class="text-muted">We offer an extremely short response time. Our locksmiths are always on the roads of Calgary, looking to provide the fastest possible response to our customers.</p>
+        </div>
+        <div class="col-5 m-1 bg-primary bg-gradient rounded shadow-sm p-4">
+            <h4 class="text-white fw-bold">Expert Locksmith Guaranteed</h4>
+            <p class="text-muted">As one of Calgary's leading auto locksmith service providers, our team of service technicians is highly skilled and professional. They will assist you no matter what car model you need keys to.</p>
+        </div>
+    </div>
+</div>
+
+
+<!-- MAP -->
+<div class="mt-5">
+    <!-- Google Map Copied Code -->
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.756619449678!2d36.78490697487992!3d-1.3218183986656415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1071345fc851%3A0x24351431a1c95b0d!2sPark%202%20Estate!5e0!3m2!1sen!2ske!4v1695247134711!5m2!1sen!2ske" 
+        width="100%" 
+        height="450" 
+        style="border:0;" 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+</div>
+<?php include "includes/footer.php"?>
