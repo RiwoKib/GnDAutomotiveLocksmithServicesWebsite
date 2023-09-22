dd7fe45d0d05ab26d8d2a49adb648b69bdd6e913
Riwokib <riwellkib@gmail.com>
2023-09-22 11:39:27 +0300

    banner | posts | contact us section

---
+++ blog.php
@@ -0,0 +1,110 @@
+<?php include "includes/header.php"?>
+
+<div class="bg-image">
+    
+    <img class="w-100 bg-img" src="assets/img/banner1.jpg">
+    
+</div>
+
+<!-- CALL TO ACTION -->
+<div class="row py-3 px-5 bg-dark justify-content-center align-items-center">
+    <div class="col-9 text-white">
+        <p>Learn from auto locksmith experts about your car's security. We share our knowledge and experience, such as cool automotive locksmith tricks, how to stay secure in the era of technology, and much more! Need a consultation? Call <strong>0742-874-651</strong> GnDAutoLocksmith you can trust.</p>
+    </div>
+</div>
+
+<!-- BLOG POSTS -->
+<div class="container">
+    <div class="row my-5">
+        <div class="col-12 text-center">
+            <h2><strong>BLog Posts</strong></h2>
+            <h5 class="text-info">Lets Learn Together</h5>
+            <hr>
+        </div>
+    </div>
+
+    <div class="row my-5 justify-content-center align-items-center">
+         
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/keyduplicate.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title text-primary"><strong>How to duplicate car keys with chips</strong></h5>
+                    <p class="card-text">Car keys duplication, everything you need to know about how to duplicate car keys with chips. Tips by a professional automotive locksmiths GnDAutoLocksmith</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/transponderkeys.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title text-primary"><strong>What to do if you lost your car Keys</strong></h5>
+                    <p class="card-text">Lost all of your car keys and do not know what to do? Learn what to do if you lost all of your car keys. Tips from a professional automotive locksmith.</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="col-4">
+            <div class="card" style="width: 25rem;">
+                <img src="assets/img/ignitionrepair.jfif" class="w-100">
+                <div class="card-body">
+                    <h5 class="card-title text-primary"><strong>Key Fob Programming and Reprogramming</strong></h5>
+                    <p class="card-text">
+                        Learn the steps invloved in programming and reprogramming key fobs for various car makes and models.
+                    </p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Read More...</a>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+
+     <!-- CONTACT US SECTION -->
+     <div class="row my-5 justify-content-center">
+        <div class="col-6">
+            <h2 class="fw-bold">Contact Us</h2>
+            <hr class="col-2">
+            <div class="row">
+                <div class="col-6">
+                <p>Have a question? Looking for a quote? Send us a message and we'll be in touch pronto.</p>
+
+                <h4 class="fw-bold">Area of Service</h4>
+                <p>Nairobi Karioko, Langata, Mbagathi Road, Jogoo Road.</p>
+
+                <h4>Business Hours</h4>
+                <p>Mon-Sat 7:00am - 7:00pm <br><strong class="text-danger">Emergency Services</strong> available.
+                <br>
+                (Open Ups)</p>
+                </div>
+                <div class="col-4 d-flex ">
+                    <div>
+                        <i class="fa text-warning fa-envelope-o fa-4x"></i>
+                        <h5 class="fw-bold text-primary">Email Us</h5>
+                        <a class="text-muted" href="">Send Us a Message</a>
+                    </div>
+                    <div class="text-nowrap">
+                        <i class="fa text-warning fa-phone fa-4x"></i>
+                        <h5 class="fw-bold text-primary">Call Us</h5>
+                        <a class="text-muted" href="">0742-874-651</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <img src="assets/img/contactlostkey.png">
+        </div>
+    </div>
+</div>
+
+<?php include "includes/footer.php"?>
